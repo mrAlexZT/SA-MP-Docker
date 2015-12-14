d74db5e26b1305429846dae3e67f957c7739f6fa
CKA3KuH <CKA3KuH@gmail.com>
2015-12-14 17:35:51 +0200

    Update prepare_os.sh

--- setup/prepare_os.sh
+++ setup/prepare_os.sh
@@ -1,1 +1,4 @@
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 
+yum update -y && yum upgrade -y
