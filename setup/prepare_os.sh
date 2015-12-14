3de03cd6e6f7963aedb0fca4ff8a1078ecf6b391
CKA3KuH <CKA3KuH@gmail.com>
2015-12-14 18:21:20 +0200

    Update prepare_os.sh

--- setup/prepare_os.sh
+++ setup/prepare_os.sh
@@ -7,8 +7,13 @@ rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 
 yum update -y && yum upgrade -y
 
-yum install mc git wget tar vsftpd net-tools tcpdump bash-completion -y
+yum install mc git wget tar vsftpd net-tools tcpdump gcc gcc-c++ make cmake libstdc++-devel compat-libstdc++-33.i686 libstdc++.i686 libstdc++-devel.i686 glibc-devel.i686 bash-completion -y
 systemctl enable vsftpd
 #service vsftpd start
+
+#RUN iptables -I INPUT -p tcp -m tcp --dport 21 -m state --state NEW -j ACCEPT
+#RUN iptables -I INPUT -p tcp -m tcp --dport 7777 -m state --state NEW -j ACCEPT
+#RUN iptables -I INPUT -p udp -m udp --dport 7777 -m state --state NEW -j ACCEPT
+#RUN service iptables save
 
 exit 0
