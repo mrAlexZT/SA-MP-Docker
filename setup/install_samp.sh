0d3d833a69f28c8dd2b4605e5d1b168253735cf6
CKA3KuH <CKA3KuH@gmail.com>
2015-12-14 18:23:21 +0200

    Create install_samp.sh

---
+++ setup/install_samp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+
+RUN cd ~ && \
+ mkdir ~/server/ && \
+ curl -OL http://files.sa-mp.com/samp037svr_R2-1.tar.gz && \
+ tar -zxf samp037svr*.tar.gz -C /tmp/ && \
+ cp -Rf /tmp/samp03/* ~/server/ && \
+ rm -rf ~/samp037svr*.tar.gz && \
+ rm -rf /tmp/samp03/
+
+RUN sed -i 's/rcon_password changeme/rcon_password Sa-MpDocker2015!/' ~/server/server.cfg
+RUN sed -i 's/hostname SA-MP 0.3 Server/hostname SA-MP 0.3 Docker Server/' ~/server/server.cfg
+RUN sed -i 's/announce 0/announce 1/' ~/server/server.cfg
+RUN sed -i 's/maxplayers 50/maxplayers 1000/' ~/server/server.cfg
+
+exit 0
