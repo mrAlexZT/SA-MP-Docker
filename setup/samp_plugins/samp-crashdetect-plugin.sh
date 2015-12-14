a04580fbefe7c8bb1bf540c20d663fce679f9143
CKA3KuH <CKA3KuH@gmail.com>
2015-12-14 19:57:03 +0200

    Update samp-crashdetect-plugin.sh

--- setup/samp_plugins/samp-crashdetect-plugin.sh
+++ setup/samp_plugins/samp-crashdetect-plugin.sh
@@ -2,6 +2,12 @@
 
 set -e
 
-
+cd /opt/setup-os/samp_plugins/
+git clone https://github.com/samp-incognito/samp-streamer-plugin.git
+cd /opt/setup-os/samp_plugins/samp-streamer-plugin/
+make
+mkdir -p ~/server/plugins/
+cp /opt/setup-os/samp_plugins/samp-streamer-plugin/bin/linux/Release/streamer.so ~/server/plugins/
+echo 'plugins streamer.so' >> ~/server/server.cfg
 
 exit 0
