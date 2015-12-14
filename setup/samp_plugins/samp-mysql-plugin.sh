a6361a165e0de63b8f8bb58a655fc38d0ef9aed8
CKA3KuH <CKA3KuH@gmail.com>
2015-12-14 20:12:55 +0200

    Update samp-mysql-plugin.sh

--- setup/samp_plugins/samp-mysql-plugin.sh
+++ setup/samp_plugins/samp-mysql-plugin.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+cd /opt/setup-os/samp_plugins/
+git clone https://github.com/pBlueG/SA-MP-MySQL.git
+cd /opt/setup-os/samp_plugins/SA-MP-MySQL/
+mkdir build && cd build
+#cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
 
+mkdir -p ~/server/plugins/
+#cp /opt/setup-os/samp_plugins/samp-streamer-plugin/bin/linux/Release/streamer.so ~/server/plugins/
+#echo 'plugins streamer.so' >> ~/server/server.cfg
 
 exit 0
