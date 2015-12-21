2bd20ae78f1d6381074f26e9f92a337e7c47726f
CKA3KuH <CKA3KuH@gmail.com>
2015-12-21 16:54:22 +0200

    Update install_samp.sh

--- setup/install_samp.sh
+++ setup/install_samp.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 cd ~ && \
  mkdir ~/server/ && \
  curl -OL http://files.sa-mp.com/samp037svr_R2-1.tar.gz && \
@@ -10,6 +9,11 @@ cd ~ && \
  cp -Rf /tmp/samp03/* ~/server/ && \
  rm -rf ~/samp037svr*.tar.gz && \
  rm -rf /tmp/samp03/
+ 
+echo 'logtimeformat [%d/%m/%Y %H:%M:%S]
+chatlogging 0
+messageholelimit 3000
+lagcompmode 1' >> ~/server/server.cfg
 
 sed -i 's/rcon_password changeme/rcon_password Sa-MpDocker2015!/' ~/server/server.cfg
 sed -i 's/hostname SA-MP 0.3 Server/hostname SA-MP 0.3 Docker Server/' ~/server/server.cfg
