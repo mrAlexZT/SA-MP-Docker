840e9e38bb9496fae45afa951a9b6449b5cdace5
mr.AlexZT <mr.AlexZT@gmail.com>
2016-04-22 21:58:58 +0300

    fix Build failed: The command /bin/sh -c /opt/setup-os/prepare_os.sh returned a non-zero code: 1

--- setup/prepare_os.sh
+++ setup/prepare_os.sh
@@ -9,7 +9,7 @@ yum update -y && yum upgrade -y
 yum install -y mc git wget tar htop bash-completion vsftpd net-tools tcpdump telnet supervisor make cmake gcc gcc-c++ libstdc++-devel compat-libstdc++-33.i686 libstdc++.i686 libstdc++-devel.i686 glibc-devel.i686
 
 systemctl enable vsftpd
-#service vsftpd start
+service vsftpd start
 
 #RUN iptables -I INPUT -p tcp -m tcp --dport 21 -m state --state NEW -j ACCEPT
 #RUN iptables -I INPUT -p tcp -m tcp --dport 7777 -m state --state NEW -j ACCEPT
