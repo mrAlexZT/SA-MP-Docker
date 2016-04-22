15750e9fa128bb0a32dd56313167bbdfffabf0dd
mrAlexZT <mr.AlexZT@gmail.com>
2016-04-22 22:49:48 +0300

    Update prepare_os.sh

--- setup/prepare_os.sh
+++ setup/prepare_os.sh
@@ -6,7 +6,7 @@ yum install -y epel-release remi-release
 
 yum update -y && yum upgrade -y
 
-yum install -y mc git wget tar htop bash-completion vsftpd net-tools tcpdump telnet supervisor make cmake gcc gcc-c++ libstdc++-devel compat-libstdc++-33.i686 libstdc++.i686 libstdc++-devel.i686 glibc-devel.i686
+yum install -y mc git wget tar htop bash-completion vsftpd net-tools tcpdump telnet supervisor make cmake gcc gcc-* libstdc++-devel compat-libstdc++-33.i686 libstdc++.i686 libstdc++-devel.i686 glibc-devel.i686
 
 systemctl enable vsftpd
 service vsftpd start > /dev/null
